2c1312e9fcb0f4487d9e11e250a9563b18008a8b
comfuture <comfuture@gmail.com>
2021-10-13 02:39:46 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Head>
+      <Title>Hello, {{ namae }}</Title>
+    </Head>
+    <div>Hello, {{ namae }}</div>
+    <button @click="setName('new world')">setName('new world')</button>
+    <button @click="setName('another world')">setName('another world')</button>
+  </div>
+</template>
+<script setup lang="ts">
+import type { Ref } from 'vue'
+
+const namae: Ref<string> = useState<string>('name', () => 'world')
+const setName = (value: string) => namae.value = value
+</script>
